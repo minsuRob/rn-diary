6647d0876314a21245f2dc43e7e9b320eebfcec9
minsu.lee <minsu.rob@gmail.com>
2024-09-25 20:40:37 +0900

    5.1: 메인화면 레이아웃 및 고정 버튼 추가

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import styled from 'styled-components/native';
-const View = styled.View``;
+import colors from '../color';
+const View = styled.View`
+  background-color: ${colors.bgColor};
+  flex: 1;
+  padding: 0px 50px;
+  padding-top: 100px;
+`;
 const Text = styled.Text`
-  color: green;
+  font-size: 35px;
+  font-weight: 700;
+`;
+const Title = styled.Text`
+  color: ${colors.textColor};
+  font-size: 38px;
+  margin-bottom: 100px;
+`;
+const Btn = styled.TouchableOpacity`
+  background-color: ${colors.btnColor};
+  border-radius: 40px;
+  height: 80px;
+  width: 80px;
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  justify-content: center;
+  align-items: center;
+  elevation: 5;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
 `;
 const Home = () => (
   <View>
-    <Text>Home</Text>
-    <Text>Home</Text>
-    <Text>Home</Text>
-    <Text>Home</Text>
+    <Title>My Jounal</Title>
+    <Btn>
+      <Text>+</Text>
+    </Btn>
   </View>
 );
 export default Home;
