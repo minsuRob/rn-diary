26bd5efedfd7091b2dddf2cfd03073d2445a9e53
minsu.lee <minsu.rob@gmail.com>
2024-09-27 00:37:16 +0900

    3.6: 트렌드 영화 세로 스크롤 css 처리

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import Poster from '@/components/Poster';
 import Slide from '@/components/Slide';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {useEffect, useState} from 'react';
@@ -18,6 +19,33 @@ const Loader = styled.View`
 
 const {height: SCREEN_HEIGHT} = Dimensions.get('window');
 
+const ListTitle = styled.Text`
+  color: white;
+  font-size: 18px;
+  font-weight: 600;
+  margin-left: 30px;
+`;
+
+const TrendingScroll = styled.ScrollView`
+  margin-top: 20px;
+`;
+
+const Movie = styled.View`
+  margin-right: 20px;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-weight: 600;
+  margin-top: 7px;
+  margin-bottom: 5px;
+`;
+const Votes = styled.Text`
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 10px;
+`;
+
 const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
@@ -34,7 +62,7 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
   const getUpcoming = async () => {
     const {results} = await (
       await fetch(
-        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`,
       )
     ).json();
     setUpcoming(results);
@@ -67,7 +95,11 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
         autoplayTimeout={5.5}
         showsButtons={false}
         showsPagination={false}
-        containerStyle={{width: '100%', height: SCREEN_HEIGHT / 4}}>
+        containerStyle={{
+          marginBottom: 30,
+          width: '100%',
+          height: SCREEN_HEIGHT / 4,
+        }}>
         {nowPlaying.map(movie => (
           <Slide
             key={movie.id}
@@ -79,6 +111,19 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
           />
         ))}
       </Swiper>
+      <ListTitle>Trending Movies</ListTitle>
+      <TrendingScroll>
+        {trending.map(movie => (
+          <Movie key={movie.id}>
+            <Poster path={movie.poster_path} />
+            <Title>
+              {movie.original_title.slice(0, 13)}
+              {movie.original_title.length > 13 ? '...' : null}
+            </Title>
+            <Votes>⭐️ {movie.vote_average}/10</Votes>
+          </Movie>
+        ))}
+      </TrendingScroll>
     </Container>
   );
 };
