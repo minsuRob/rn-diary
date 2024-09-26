5bce8393feab6ea6fcd74a76700d3d49cc68427a
minsu.lee <minsu.rob@gmail.com>
2024-09-26 20:09:42 +0900

    fix: 기본 swiper 내용 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,41 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import React from 'react';
+import {Dimensions} from 'react-native';
+import Swiper from 'react-native-web-swiper';
+import styled from 'styled-components/native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const API_KEY = '10923b261ba94d897ac6b81148314a3f';
 
-export default function HomeScreen() {
+const Container = styled.ScrollView`
+  background-color: ${props => props.theme.mainBgColor};
+`;
+
+const View = styled.View`
+  flex: 1;
+`;
+
+const {height: SCREEN_HEIGHT} = Dimensions.get('window');
+
+const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
+  const getNowPlaying = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`,
+    );
+  };
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <Container>
+      <Swiper
+        loop
+        timeout={3.5}
+        controlsEnabled={false}
+        containerStyle={{width: '100%', height: SCREEN_HEIGHT / 4}}>
+        <View style={{backgroundColor: 'red'}}></View>
+        <View style={{backgroundColor: 'blue'}}></View>
+        <View style={{backgroundColor: 'red'}}></View>
+        <View style={{backgroundColor: 'blue'}}></View>
+      </Swiper>
+    </Container>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default Movies;
---
+++ screens/Movies.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {NativeStackScreenProps} from 'react-native-screens/lib/typescript/native-stack/types';
+import styled from 'styled-components/native';
+
+const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => null;
+
+export default Movies;
