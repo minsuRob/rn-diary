1f2bf6579cacb1c976de6e4c123c18e43161b81f
minsu.lee <minsu.rob@gmail.com>
2024-09-30 22:44:09 +0900

    3.16: tv 최신 데이터 로드

--- app/(tabs)/tv.tsx
+++ app/(tabs)/tv.tsx
@@ -1,10 +1,69 @@
+import {tvApi} from '@/api';
 import Loader from '@/components/Loader';
+import VMedia from '@/components/VMedia';
 import React from 'react';
-import {Text} from 'react-native';
+import {FlatList, ScrollView, Text} from 'react-native';
 import {NativeStackScreenProps} from 'react-native-screens/lib/typescript/native-stack/types';
+import {useQuery} from 'react-query';
 
-const TVScreen: React.FC<NativeStackScreenProps<any, 'TV'>> = () => {
-  return <Text>What</Text>;
+const Tv = () => {
+  const {isLoading: todayLoading, data: todayData} = useQuery(
+    ['tv', 'today'],
+    tvApi.airingToday,
+  );
+  const {isLoading: topLoading, data: topData} = useQuery(
+    ['tv', 'top'],
+    tvApi.topRated,
+  );
+  const {isLoading: trendingLoading, data: trendingData} = useQuery(
+    ['tv', 'trending'],
+    tvApi.trending,
+  );
+  const loading = todayLoading || topLoading || trendingLoading;
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <ScrollView>
+      <FlatList
+        data={trendingData.results}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => (
+          <VMedia
+            posterPath={item.poster_path}
+            originalTitle={item.original_name}
+            voteAverage={item.vote_average}
+          />
+        )}
+      />
+      <FlatList
+        data={todayData.results}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => (
+          <VMedia
+            posterPath={item.poster_path}
+            originalTitle={item.original_name}
+            voteAverage={item.vote_average}
+          />
+        )}
+      />
+      <FlatList
+        data={topData.results}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => (
+          <VMedia
+            posterPath={item.poster_path}
+            originalTitle={item.original_name}
+            voteAverage={item.vote_average}
+          />
+        )}
+      />
+    </ScrollView>
+  );
 };
 
-export default TVScreen;
+export default Tv;
