789d8f64eb9a39e0cdbeb60a0418149360db2583
minsu.lee <minsu.rob@gmail.com>
2024-11-20 00:58:39 +0900

    온보딩 css 초안

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,41 +1,37 @@
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {Dimensions} from 'react-native';
-import Swiper from 'react-native-web-swiper';
-import styled from 'styled-components/native';
+import {StyleSheet, Image, Dimensions, Text, View} from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Swiper from 'react-native-swiper';
+// import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from '@expo-google-fonts/montserrat';
 
-const API_KEY = '10923b261ba94d897ac6b81148314a3f';
+const h = Dimensions.get('window').width;
+const w = Dimensions.get('window').height;
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+  },
+});
 
-const Container = styled.ScrollView`
-  background-color: ${props => props.theme.mainBgColor};
-`;
-
-const View = styled.View`
-  flex: 1;
-`;
-
-const {height: SCREEN_HEIGHT} = Dimensions.get('window');
-
-const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
-  const getNowPlaying = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`,
-    );
-  };
+export default function TabTwoScreen() {
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
   return (
-    <Container>
-      <Swiper
-        loop
-        timeout={3.5}
-        controlsEnabled={false}
-        containerStyle={{width: '100%', height: SCREEN_HEIGHT / 4}}>
-        <View style={{backgroundColor: 'red'}}></View>
-        <View style={{backgroundColor: 'blue'}}></View>
-        <View style={{backgroundColor: 'red'}}></View>
-        <View style={{backgroundColor: 'blue'}}></View>
-      </Swiper>
-    </Container>
+    <View>
+      <AntDesign name="arrowright" size={22} />
+    </View>
+    // <Swiper
+    //   nextButton={
+    //   }></Swiper>
   );
-};
-
-export default Movies;
+}
