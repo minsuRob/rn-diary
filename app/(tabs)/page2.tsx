76341a292859bba7cbe363e4c1fe9ec105ab1240
minsu.lee <minsu.rob@gmail.com>
2024-11-22 02:11:17 +0900

    초기 페이지 추가

---
+++ app/(tabs)/page2.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet, Image, Text, View} from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Feather from '@expo/vector-icons/Feather';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+export default function TabTwoScreen() {
+  return (
+    <>
+      <Text>good</Text>
+      <Text>good</Text>
+      <Text>good</Text>
+      <Text>good</Text>
+      <Text>good</Text>
+      <Text>good</Text>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {},
+  titleContainer: {},
+});
