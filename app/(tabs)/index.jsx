feaea1387ffc728e764560d53f50331276f8e047
minsu.lee <minsu.rob@gmail.com>
2024-11-22 11:58:35 +0900

    fix: 기본화면 추가

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -6,6 +6,7 @@ import {
   Text,
   View,
   TouchableOpacity,
+  ImageBackground,
 } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Swiper from 'react-native-swiper';
@@ -39,44 +40,13 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginTop: 20,
   },
-  detail: {
-    color: '#FFF',
-    fontFamily: 'Montserrat_400Regular',
-    fontSize: 18,
-    textAlign: 'center',
-    paddingHorizontal: 20,
-    lineHeight: 30,
-    marginTop: 30,
-  },
-  btn: {
-    marginTop: 80,
-    backgroundColor: '#E2443B',
-    paddingHorizontal: 140,
-    paddingVertical: 10,
-    borderRadius: 30,
-  },
-  text: {
-    fontFamily: 'Montserrat_600SemiBold',
-    fontSize: 30,
-    color: '#FFF',
-  },
 });
 
 const App = props => {
   return (
-    <View style={styles.container}>
-      <Image source={require('@/images/motorcycle1.png')} style={styles.img} />
-      <Text style={styles.title}>Maxx Scooter</Text>
-      <Text style={styles.detail}>
-        With an updated motor, and integrated anti-theft tech the maxx scooters
-        are custom-tuned for the ultimate riding experience.
-      </Text>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => router.push('/page2')}>
-        <Text style={styles.text}>Next</Text>
-      </TouchableOpacity>
-    </View>
+    <ImageBackground
+      source={require('@/images/home.png')}
+      style={{width: '100%', height: '100%'}}></ImageBackground>
   );
 };
 
